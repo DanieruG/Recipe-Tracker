905515778fde54ef2e2942dba9c556a7e9f3719c
DanieruG <danielgyabaah952@outlook.com>
2026-02-19 20:46:23 +0000

    First iteration of scheduling algorithm

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -7,6 +7,21 @@ import { recipeSchema, scheduleSchema } from "@/types/form";
 type recipe = z.infer<typeof recipeSchema>
 type custom = z.infer<typeof scheduleSchema>
 
+// This is a Fisher-Yates shuffle.
+function shuffler(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
+
+function getDayName(index: number): string {
+    const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    return daysOfWeek[index % 7];
+}
+
 export async function createRecipe(formData: recipe){
 
     const ingredients = formData.coreIngredients
@@ -56,7 +71,7 @@ export async function createSchedule(formData: custom){
             checkValid: checkTrue,
             errors: z.flattenError(result.error)
         }
-    } else {
+    }
         //   First; collect recipes   that are one of the  selected.
         // Prisma results return an array of objects.
         // Collect  which options are enabled
@@ -64,6 +79,7 @@ export async function createSchedule(formData: custom){
             formData.dinnerEnable && "Dinner" , 
             formData.lunchEnable && "Lunch"
         ].filter(Boolean) as string[]
+
 
         //  Factors: meal type, and ingredients.
         const recipes =  await prisma.recipe.findMany({
@@ -83,15 +99,53 @@ export async function createSchedule(formData: custom){
                 
         }})
 
-        return {
-            success: true,
-            validRecipes: recipes
+        const breakfast_options = shuffler(recipes.filter((c) => c.mealType == "Breakfast"))
+        const lunch_options = shuffler(recipes.filter((c) => c.mealType == "Lunch"))
+        const dinner_options = shuffler(recipes.filter((c) => c.mealType == "Dinner"))
+        // First, filter the recipes. Then, distribute across 7 days.
+        // Need a mealPlan type, which will be made up of  breakfast, lunch and dinner.
+        type mealPlan = {
+            breakfast?: recipe,
+            lunch?: recipe,
+            dinner?: recipe
         }
+
+        // This will store the day, and the meals for that day.
+        type dayPlan = {
+            day: string,
+            meals: mealPlan
+        }
+
+        const weeklyPlan: dayPlan[] = []
+        // Now, construct the weekly plan based on the available recipes.
+
+        for (let i = 0; i < 7; i++){
+            const newMealPlan: mealPlan = {
+                breakfast: formData.breakfastEnable ? breakfast_options[
+                    i % breakfast_options.length
+                ] : undefined,
+                lunch: formData.lunchEnable ? lunch_options[
+                    i % lunch_options.length
+                ] : undefined,
+                dinner: formData.dinnerEnable ? dinner_options[
+                    i % dinner_options.length
+                ] : undefined
+            }
+            // This builds an object, with mod being used to prevent index out of bounds.
+
+            weeklyPlan.push({
+                day: getDayName(i),
+                meals: newMealPlan
+            })
+        }
+
+         return {
+            success: true,
+            validRecipes: weeklyPlan
+         }
     }
 
     // If parsed successfully... then create a schedule.
     // All schedule consist of 7 days; lunch only appears on select days.
     // Must start from the shopping day.
     // But first, collect all recipes matching plan criteria.
-
-}
